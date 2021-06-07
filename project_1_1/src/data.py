2a652addc88c6e39e653d93c9881c074ae1ac263
Jonas Christophersen <59091779+jonassoebro@users.noreply.github.com>
2021-06-07 14:02:04 +0200

    Update data.py

--- project_1_1/src/data.py
+++ project_1_1/src/data.py
@@ -1,3 +1,17 @@
+import os
+import numpy as np
+import glob
+import PIL.Image as Image
+from tqdm.notebook import tqdm
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+
 def download_data():
   if not os.path.exists('./hotdog_nothotdog'):
     import gdown
