48771573168a342c18d040c93b2404f51a353263
Till <tillaczel@gmail.com>
2021-06-07 19:33:25 +0200

    WIP 6

---
+++ project_1_1/src/trainer.py
@@ -0,0 +1,26 @@
+import torch
+
+
+def get_trainer(cfg, engine):
+    callbacks = list()
+    callbacks.append(pl.callbacks.EarlyStopping(patience=cfg.training.early_stopping.stopping_patience,
+                                                monitor=cfg.training.early_stopping.monitor))
+    callbacks.append(pl.callbacks.ModelCheckpoint(dirpath=wandb.run.dir,
+                                                  monitor=cfg.training.model_checkpoint.monitor,
+                                                  filename='model',
+                                                  verbose=True,
+                                                  period=1))
+
+    gpus = 0
+    if torch.cuda.is_available():
+        gpus = -1
+
+    logger = pl.loggers.WandbLogger()
+    logger.watch(engine)
+
+    # TODO: we can use this to put configuration into nice table in wandb
+    # logger.log_hyperparams(hyperparams)
+
+    trainer = pl.Trainer(callbacks=callbacks, logger=logger, default_root_dir="training/logs",
+                         max_epochs=cfg.training.max_epochs, gpus=gpus)
+    return trainer
--- project_1_1/train.py
+++ project_1_1/train.py
@@ -4,14 +4,12 @@ import os
 sys.path.append('git_repo')
 
 import hydra
-import torch
 import wandb
 from omegaconf import DictConfig, OmegaConf
-import pytorch_lightning as pl
 
-from project_1_1.src.data import get_data
+from project_1_1.src.data import get_data, download_data
 from project_1_1.src.engine import EngineModule
-from project_1_1.src.data import download_data
+from project_1_1.src.trainer import get_trainer
 
 wandb.init(project='p1', entity='dlcv')
 
@@ -41,26 +39,3 @@ if __name__ == '__main__':
     run_training()
 
 
-def get_trainer(cfg, engine):
-    callbacks = list()
-    callbacks.append(pl.callbacks.EarlyStopping(patience=cfg.training.early_stopping.stopping_patience,
-                                                monitor=cfg.training.early_stopping.monitor))
-    callbacks.append(pl.callbacks.ModelCheckpoint(dirpath=wandb.run.dir,
-                                                  monitor=cfg.training.model_checkpoint.monitor,
-                                                  filename='model',
-                                                  verbose=True,
-                                                  period=1))
-
-    gpus = 0
-    if torch.cuda.is_available():
-        gpus = -1
-
-    logger = pl.loggers.WandbLogger()
-    logger.watch(engine)
-
-    # TODO: we can use this to put configuration into nice table in wandb
-    # logger.log_hyperparams(hyperparams)
-
-    trainer = pl.Trainer(callbacks=callbacks, logger=logger, default_root_dir="training/logs",
-                         max_epochs=cfg.training.max_epochs, gpus=gpus)
-    return trainer
