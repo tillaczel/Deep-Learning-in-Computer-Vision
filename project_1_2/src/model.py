eef1f7c3d1ffcb17ba52eed165e302b2ee11826f
Till <tillaczel@gmail.com>
2021-06-09 15:37:59 +0200

    conv

--- project_1_2/src/model.py
+++ project_1_2/src/model.py
@@ -1,5 +1,5 @@
 import torchvision.models as models
-from torch.nn import Module, Sequential, Linear, Softmax
+from torch.nn import Module, Sequential, Conv2d
 
 
 class Model(Module):
@@ -7,7 +7,7 @@ class Model(Module):
     def __init__(self, pretrained: bool = False, in_dim: int = 2048, out_dim: int = 256):
         super(Model, self).__init__()
         self.resnet = Sequential(*list(models.resnet50(pretrained=pretrained).children())[:-1])
-        self.linear = Linear(in_features=in_dim, out_features=out_dim, bias=True)
+        self.linear = Conv2d(in_dim, out_dim, (1, 1), bias=True)
 
     def forward(self, x):
         x = self.resnet(x)
