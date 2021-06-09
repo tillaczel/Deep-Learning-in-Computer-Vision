4f2267d2e67fc29c7fff78bd76be637e86a14dee
Till <tillaczel@gmail.com>
2021-06-09 15:40:00 +0200

    fix 1

--- project_1_2/src/model.py
+++ project_1_2/src/model.py
@@ -7,10 +7,9 @@ class Model(Module):
     def __init__(self, pretrained: bool = False, in_dim: int = 2048, out_dim: int = 256):
         super(Model, self).__init__()
         self.resnet = Sequential(*list(models.resnet50(pretrained=pretrained).children())[:-1])
-        self.linear = Conv2d(in_dim, out_dim, (1, 1), bias=True)
+        self.conv = Conv2d(in_dim, out_dim, (1, 1), bias=True)
 
     def forward(self, x):
         x = self.resnet(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear(x)
+        x = self.conv(x)
         return x
