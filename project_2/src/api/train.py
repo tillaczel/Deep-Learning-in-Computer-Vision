95e8a8e35f0085eacd7e01b37139afd5761bed83
Till <tillaczel@gmail.com>
2021-06-14 16:41:57 +0200

    WIP 4

--- project_2/src/api/train.py
+++ project_2/src/api/train.py
@@ -17,13 +17,16 @@ def run_training(cfg: DictConfig):
     wandb.save('*.ckpt')  # should keep it up to date
     trainer = get_trainer(cfg, engine)
 
+    # Todo: save models in wandb
     if cfg.model.ensemble:
+        model_path = 'models'
+        os.mkdir(model_path) if not os.path.isdir(model_path) else None
         for i in range(4):
             train_loader, valid_loader, test_loader = \
                 get_dataloaders(cfg.data.size, cfg.data.train_augmentation, cfg.training.batch_size, cfg.data.url,
                                 cfg.data.path, i)
             trainer.fit(engine, train_dataloader=train_loader, val_dataloaders=valid_loader)
-            trainer.save_checkpoint(f"example_{i}.ckpt")
+            trainer.save_checkpoint(os.path.join(model_path, f"example_{i}.ckpt"))
     else:
         train_loader, valid_loader, test_loader = \
             get_dataloaders(cfg.data.size, cfg.data.train_augmentation, cfg.training.batch_size, cfg.data.url,
