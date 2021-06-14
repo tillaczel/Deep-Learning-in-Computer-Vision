732bee76634f387bba30acbf91ea06369f700bc3
Till <tillaczel@gmail.com>
2021-06-14 16:55:15 +0200

    WIP 1

---
+++ project_2/src/metrics/__init__.py
@@ -0,0 +1,25 @@
+import torchmetrics
+
+from project_2.src.metrics.dice import Dice
+from project_2.src.metrics.iou import IoU
+
+
+class Metrics:
+    def __init__(self, main_metrics):
+        # TODO: instance average those?
+        self.train_acc = torchmetrics.Accuracy(multiclass=False)
+        self.val_acc = torchmetrics.Accuracy(multiclass=False)
+
+        self.train_sensitivity = torchmetrics.Recall(multiclass=False)
+        self.val_sensitivity = torchmetrics.Recall(multiclass=False)
+        self.train_specificity = torchmetrics.Specificity(multiclass=False)
+        self.val_specificity = torchmetrics.Specificity(multiclass=False)
+
+        self.train_iou = IoU()
+        self.val_iou = IoU()
+
+        self.train_dice = Dice()
+        self.val_dice = Dice()
+
+        self.metrics = ["acc", "sensitivity", "specificity", "iou", "dice"]
+        self.main_metrics = main_metrics
