04966105570014e737773b625b643a537c74893e
Jonas Christophersen <59091779+jonassoebro@users.noreply.github.com>
2021-06-14 15:53:38 +0200

    Update plot_results.py

--- project_2/src/plot_results.py
+++ project_2/src/plot_results.py
@@ -3,8 +3,8 @@ from IPython.display import clear_output
 
 def plot_predictions(input_data, predictions): 
     plt.rcParams['figure.figsize'] = [18, 6]
-    plt.imshow(input_data)
-    plt.imshow(predictions)
+    plt.imshow(torch.squeeze(input_data,0))
+    plt.imshow(torch.squeeze(predictions,0))
     
     #for i in range(6):
     #    plt.subplot(2, 6, i+1)
