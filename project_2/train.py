74357d290c70ad99c7cf20c62aa0530affd0ac61
Till <tillaczel@gmail.com>
2021-06-14 11:50:37 +0200

    WIP 7

--- project_2/train.py
+++ project_2/train.py
@@ -7,7 +7,7 @@ from omegaconf import DictConfig, OmegaConf
 sys.path.append('git_repo')
 sys.path.append(os.path.split(os.getcwd())[0])
 
-from project_2.src.api import run_training
+from project_2.src.api.train import run_training
 
 wandb.init(project='p2', entity='dlcv')
 
