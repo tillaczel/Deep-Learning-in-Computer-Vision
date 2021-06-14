dff1f6d3c1f2acbb3be7b30c2d5a29019adcff48
Till Aczél <tillaczel@gmail.com>
2021-06-14 11:07:13 +0200

    Till/model (#32)
    * WIP 1
    
    * WIP 2
    
    * WIP 3
    
    * WIP 4
    
    * WIP 5
    
    * WIP 6
    
    * WIP 7
    
    * WIP 8
    
    * WIP 9

--- project_2/src/model.py
+++ project_2/src/model.py
@@ -6,7 +6,7 @@ from .unet_parts import *
 
 class Model(nn.Module):
     def __init__(self, n_channels, n_classes, bilinear=True):
-        super(UNet, self).__init__()
+        super(Model, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
