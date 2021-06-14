286046aabd1fe574a6f603fb60059d1b97038cfe
Jonas Christophersen <59091779+jonassoebro@users.noreply.github.com>
2021-06-14 15:51:22 +0200

    Update plot_results.py

--- project_2/src/plot_results.py
+++ project_2/src/plot_results.py
@@ -3,10 +3,13 @@ from IPython.display import clear_output
 
 def plot_predictions(input_data, predictions): 
     plt.rcParams['figure.figsize'] = [18, 6]
-    for i in range(6):
-        plt.subplot(2, 6, i+1)
-        plt.imshow(input_data[i])
-
-        plt.subplot(2, 6, i+7)
-        plt.imshow(predictions[i])
+    plt.imshow(input_data[i])
+    plt.imshow(predictions[i])
+    
+    #for i in range(6):
+    #    plt.subplot(2, 6, i+1)
+     #   plt.imshow(input_data[i])
+#
+#        plt.subplot(2, 6, i+7)
+#        plt.imshow(predictions[i])
     plt.show();
