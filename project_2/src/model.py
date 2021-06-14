257f625d0bc22cd62f76c61c3428d528de7de365
Jonas Christophersen <59091779+jonassoebro@users.noreply.github.com>
2021-06-14 10:43:08 +0200

    Jonas/model (#31)
    * Update model.py
    
    * Rename model.py to unet_parts.py
    
    * Create unet_model.py
    
    * Update unet_model.py
    
    * Rename unet_model.py to model.py
    
    * Update model.py

--- project_2/src/model.py
+++ project_2/src/model.py
@@ -1,16 +1,37 @@
-import torchvision.models as models
-from torch.nn import Module, Sequential, Conv2d
+#Full assembly of the parts to form the complete network
+import torch.nn.functional as F
+
+from .unet_parts import *
 
 
-class Model(Module):
+class Model(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
 
-    def __init__(self, pretrained: bool = False, in_dim: int = 2048, out_dim: int = 256):
-        super(Model, self).__init__()
-        # self.resnet = Sequential(*list(models.resnet50(pretrained=pretrained).children())[:-1])
-        self.resnet = Sequential(*list(models.resnet50(pretrained=pretrained).children())[:-1]) # maybe ?
-        self.conv = Conv2d(in_dim, out_dim, (1, 1), bias=True)
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
-        x = self.resnet(x)
-        x = self.conv(x)
-        return x
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
