c359baf63257bd1eec1cbea33acae023d08629ad
Till <tillaczel@gmail.com>
2021-06-14 17:31:32 +0200

    WIP 1

--- project_2/src/plot_results.py
+++ project_2/src/plot_results.py
@@ -1,14 +1,35 @@
 import matplotlib.pyplot as plt
-from IPython.display import clear_output
-import numpy as np
+import torch
+import os
+import wandb
 
-def plot_predictions(input_data, predictions): 
-    plt.rcParams['figure.figsize'] = [18, 6]
+
+def plot_predictions(dataset, model, device, n=6):
+    input_data, segmentations, predictions = get_data(dataset, model, device, n=n)
+
+    fig, axs = plt.subplots(3, 5, figsize=(n*5, 15))
     for i in range(6):
-        plt.axis('off')
-        plt.subplot(2, 6, i+1)
-        plt.imshow(np.squeeze(input_data[i],0),cmap="gray")
-        plt.subplot(2, 6, i+7)
-        plt.axis('off')
-        plt.imshow(np.squeeze(predictions[i],0),cmap="gray")
-    plt.savefig('foo.png');
+        axs[i, 0].imshow(input_data[i], cmap="gray")
+        axs[i, 0].set_title(f"Input")
+        axs[i, 0].axis('off')
+
+        axs[i, 0].imshow(segmentations[i], cmap="gray")
+        axs[i, 0].set_title(f"Segmentation")
+        axs[i, 0].axis('off')
+
+        axs[i, 0].imshow(predictions[i], cmap="gray")
+        axs[i, 0].set_title(f"Prediction")
+        axs[i, 0].axis('off')
+
+    fname = os.path.join(wandb.run.dir, 'preds.png')
+    plt.savefig(fname)
+    wandb.save(fname)
+
+
+def get_data(dataset, model, device, n):
+    images, segmentations = dataset[0:n]
+    images, segmentations = map(torch.unsqueeze, [images, segmentations], [0, 0])
+    preds = model(images.to(device))  # Do a forward pass of validation data to get predictions
+
+    return images.detach().cpu().numpy(), segmentations.detach().cpu().numpy(), preds.detach().cpu().numpy()
+
