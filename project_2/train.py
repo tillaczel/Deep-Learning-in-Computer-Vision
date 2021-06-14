1e4bcb81d73dba6f9f6b55ad538edcd848b13f60
Till <tillaczel@gmail.com>
2021-06-14 11:29:37 +0200

    WIP 2

--- project_2/train.py
+++ project_2/train.py
@@ -1,10 +1,10 @@
 import sys
 import os
 
-from project_2.src.api.train import run_training
-
 sys.path.append('git_repo')
 sys.path.append(os.path.split(os.getcwd())[0])
+
+from project_2.src.api import run_training
 
 if __name__ == '__main__':
     run_training()
