4e1f2e571a47db659959a4cebb42c1e44aec8e62
Jonas Christophersen <59091779+jonassoebro@users.noreply.github.com>
2021-06-14 16:08:47 +0200

    Update plot_results.py

--- project_2/src/plot_results.py
+++ project_2/src/plot_results.py
@@ -4,13 +4,9 @@ import torch
 
 def plot_predictions(input_data, predictions): 
     plt.rcParams['figure.figsize'] = [18, 6]
-    plt.imshow(torch.squeeze(input_data,0))
-    plt.imshow(torch.squeeze(predictions,0))
-    
-    #for i in range(6):
-    #    plt.subplot(2, 6, i+1)
-     #   plt.imshow(input_data[i])
-#
-#        plt.subplot(2, 6, i+7)
-#        plt.imshow(predictions[i])
+    for i in range(6):
+        plt.subplot(2, 6, i+1)
+        plt.imshow(torch.squeeze(input_data[i],0))
+        plt.subplot(2, 6, i+7)
+        plt.imshow(torch.squeeze(predictions[i],0))
     plt.show();
