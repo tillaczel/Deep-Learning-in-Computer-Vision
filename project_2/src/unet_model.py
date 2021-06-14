07523d7ff416776d33522711ab2bce25b2a8c630
Jonas Christophersen <59091779+jonassoebro@users.noreply.github.com>
2021-06-14 09:49:55 +0200

    Update unet_model.py

--- project_2/src/unet_model.py
+++ project_2/src/unet_model.py
@@ -1,5 +1,4 @@
-""" Full assembly of the parts to form the complete network """
-
+#Full assembly of the parts to form the complete network
 import torch.nn.functional as F
 
 from .unet_parts import *
