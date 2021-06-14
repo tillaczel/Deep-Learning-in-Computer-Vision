90022661f92694cb5bdc04160a1e3dc951b6a89a
Jonas Christophersen <59091779+jonassoebro@users.noreply.github.com>
2021-06-14 15:46:47 +0200

    Update plot_results.py

--- project_2/src/plot_results.py
+++ project_2/src/plot_results.py
@@ -5,8 +5,8 @@ def plot_predictions(input_data, predictions):
     plt.rcParams['figure.figsize'] = [18, 6]
     for i in range(6):
         plt.subplot(2, 6, i+1)
-        plt.imshow(X[i])
+        plt.imshow(input_data[i])
 
         plt.subplot(2, 6, i+7)
-        plt.imshow(Y[i])
+        plt.imshow(predictions[i])
     plt.show();
