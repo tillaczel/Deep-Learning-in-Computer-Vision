43e9f05619bd26c30ca95057b7f80f3cb11b55b5
Till <tillaczel@gmail.com>
2021-06-15 16:48:37 +0200

    WIP 39

--- project_2/src/metrics/compare.py
+++ project_2/src/metrics/compare.py
@@ -7,6 +7,11 @@ from . import calc_all_metrics
 def calc_inner_expert(loader):
     loader = DataLoader(loader.dataset, batch_size=len(loader.dataset), shuffle=False, num_workers=2)
     img, seg = next(iter(loader))
+    import matplotlib.pyplot as plt
+    for i in range(10):
+        a = torch.mean(seg[i], 0, keepdim=1)
+        plt.imshow(a[0])
+        plt.show()
     results = get_metrics(seg, seg)
     print('Inner expert', results)
     return results
