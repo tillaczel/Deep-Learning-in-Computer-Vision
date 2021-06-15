4c4f4ced33c6e2b8d81bbcbba97e574f6aad59b9
Till <tillaczel@gmail.com>
2021-06-15 10:43:43 +0200

    WIP 3

--- project_2/src/engine.py
+++ project_2/src/engine.py
@@ -37,7 +37,7 @@ class EngineModule(pl.LightningModule):
     def training_step(self, batch, batch_idx):
         images, labels = batch
         seg_hat = self.model(images)
-        loss = self.loss_func(seg_hat.flatten(), labels.type(torch.float32).flatten())
+        loss = self.loss_func(seg_hat, labels.type(torch.float32))
 
         self.log('loss', loss, on_step=False, on_epoch=True,
                  prog_bar=False, logger=True)
@@ -56,7 +56,7 @@ class EngineModule(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         images, labels = batch
         seg_hat = self.model(images)
-        loss = self.loss_func(seg_hat.flatten(), labels.type(torch.float32).flatten())
+        loss = self.loss_func(seg_hat, labels.type(torch.float32))
 
         self.log('val_loss', loss, on_step=False, on_epoch=True,
                  prog_bar=False, logger=True)
