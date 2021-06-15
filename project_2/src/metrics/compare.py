6bf37b3bc59daac0fc75fe2b3fdeada6226d4b72
Till <tillaczel@gmail.com>
2021-06-15 16:29:55 +0200

    WIP 34

--- project_2/src/metrics/compare.py
+++ project_2/src/metrics/compare.py
@@ -22,6 +22,7 @@ def get_metrics(pred, seg):
         for j in range(i+1, 4):
             print(pred.shape)
             print(pred[:, i].shape, seg[:, j].shape)
+            print(pred[:, i])
             result = calc_all_metrics(pred[:, i], seg[:, j].to(torch.int))
             for k, v in result.items():
                 if k in results.keys():
