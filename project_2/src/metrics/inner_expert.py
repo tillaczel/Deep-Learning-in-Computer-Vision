49b552ba9c02a0fac8438418aefbe69f0e4b71b2
Till <tillaczel@gmail.com>
2021-06-15 15:51:04 +0200

    WIP 22

--- project_2/src/metrics/inner_expert.py
+++ project_2/src/metrics/inner_expert.py
@@ -6,11 +6,15 @@ from . import calc_all_metrics
 
 def calc_inner_expert(loader):
     loader = DataLoader(loader.dataset, batch_size=len(loader.dataset), shuffle=False, num_workers=2)
-    results = list()
+    results = dict()
     for img, seg in loader:
         for i in range(3):
             for j in range(i+1, 4):
-                results.append(calc_all_metrics(seg[0], seg[1].to(torch.int)))
-    results
+                result = calc_all_metrics(seg[0], seg[1].to(torch.int))
+                for v, k in result.items():
+                    if k in results.keys():
+                        results[k] += v/6
+                    else:
+                        results[k] = v/6
     print(results)
 
