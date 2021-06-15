693f9a40074e0d95620d57f5b641fa4d575dd24c
Till <tillaczel@gmail.com>
2021-06-15 15:05:57 +0200

    WIP 4

--- project_2/src/api/eval.py
+++ project_2/src/api/eval.py
@@ -2,9 +2,9 @@ from omegaconf import DictConfig, OmegaConf
 import os
 import wandb
 
-from project_2.src.utils import download_file
-from project_2.src.engine import EngineModule
-#from project_2.src.data import get_dataloaders
+#from project_2.src.utils import download_file
+#from project_2.src.engine import EngineModule
+from project_2.src.data import get_dataloaders
 
 
 def run_eal(cfg: DictConfig):
@@ -17,9 +17,9 @@ def run_eal(cfg: DictConfig):
         fh.write(OmegaConf.to_yaml(cfg))
     wandb.save(cfg_file, base_path=wandb.run.dir)  # this will force sync it
 
-    # train_loader, valid_loader, test_loader = \
-    #     get_dataloaders(train_cfg.data.size, train_cfg.data.train_augmentation, train_cfg.training.batch_size,
-    #                     train_cfg.data.url, train_cfg.data.path, 'all')
+    train_loader, valid_loader, test_loader = \
+        get_dataloaders(train_cfg.data.size, train_cfg.data.train_augmentation, train_cfg.training.batch_size,
+                        train_cfg.data.url, train_cfg.data.path, 'all')
 
     download_file(cfg.run_id, "model.ckpt")
     engine = EngineModule.load_from_checkpoint("model.ckpt", config=train_cfg)
