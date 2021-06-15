0614492f2cc534ceca271c3212ec0c1a9493ea8f
Till <tillaczel@gmail.com>
2021-06-15 16:00:32 +0200

    WIP 25

--- project_2/src/metrics/inner_expert.py
+++ project_2/src/metrics/inner_expert.py
@@ -11,9 +11,7 @@ def calc_inner_expert(loader):
         for i in range(3):
             for j in range(i+1, 4):
                 result = calc_all_metrics(seg[0], seg[1].to(torch.int))
-                print(result)
-                for v, k in result.items():
-                    print(v)
+                for k, v in result.items():
                     if k in results.keys():
                         results[k] += v/6
                     else:
