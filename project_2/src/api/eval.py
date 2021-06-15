1501491b81a99cf5c3016fd78af0b11f1d8d8c7c
Till <tillaczel@gmail.com>
2021-06-15 21:57:41 +0200

    WIP

--- project_2/src/api/eval.py
+++ project_2/src/api/eval.py
@@ -29,7 +29,7 @@ def run_eval(cfg: DictConfig):
 
     # calc_inner_expert(test_loader)
 
-    if cfg.is_ensemble:
+    if cfg.is_ensemble or True:
         models = get_ensemble_models(cfg.run_id, train_cfg)
         preds, segs = get_ensemble_preds(test_loader, models)
         print("Ensemble scores:")
