117662c9db82c5a95fd53f93db612dbc3632661a
Till <tillaczel@gmail.com>
2021-06-15 16:14:03 +0200

    WIP 26

---
+++ project_2/src/metrics/compare.py
@@ -0,0 +1,25 @@
+import torch
+from torch.utils.data import DataLoader
+
+from . import calc_all_metrics
+
+
+def calc_inner_expert(loader):
+    loader = DataLoader(loader.dataset, batch_size=len(loader.dataset), shuffle=False, num_workers=2)
+    for img, seg in loader:
+        results = get_metrics(seg, seg)
+    print('Inner expert', results)
+    return results
+
+
+def get_metrics(pred, seg):
+    results = dict()
+    for i in range(3):
+        for j in range(i+1, 4):
+            result = calc_all_metrics(pred[0], seg[1].to(torch.int))
+            for k, v in result.items():
+                if k in results.keys():
+                    results[k] += v/6
+                else:
+                    results[k] = v/6
+    return results
