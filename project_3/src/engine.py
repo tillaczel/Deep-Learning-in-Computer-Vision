1b63718eba83b066f5ded481ff2d0a0d24baec60
Till Aczél <tillaczel@gmail.com>
2021-06-18 11:30:55 +0200

    Add optim (#57)
    * Add optim
    
    * automatic_optimization = False
    
    * 3 to 2 omptims

--- project_3/src/engine.py
+++ project_3/src/engine.py
@@ -2,6 +2,7 @@ from omegaconf import DictConfig
 import pytorch_lightning as pl
 import torch
 from torch import nn
+
 from project_3.src.model import Model
 from project_3.src.plot_results import plot_predictions
 from project_3.src.metrics import Metrics
@@ -10,15 +11,13 @@ from project_3.src.loss import Losses
 
 class EngineModule(pl.LightningModule):
 
-    def __init__(self, config: DictConfig, main_metrics=None):
+    def __init__(self, config: DictConfig):
         super().__init__()
         self.config = config
-        self.model = Model(n_channels=config.model.in_dim, n_classes=config.model.out_dim, dropout_rate=config.model.dropout_rate)
+        self.G_A2B, self.G_B2A, self.D_A, self.D_B = None, None, None, None
         self.losses = Losses()
-        if main_metrics is not None:
-            self.metrics = Metrics(main_metrics)
-        else:
-            self.metrics = Metrics()
+
+        self.automatic_optimization = False
 
     @property
     def lr(self):
@@ -36,6 +35,7 @@ class EngineModule(pl.LightningModule):
                  on_epoch=True, logger=True)
 
     def training_step(self, batch, batch_idx):
+        g_opt, d_opt, = self.optimizers()
         images, labels = batch
         seg_hat = self.model(images)
         loss = self.loss_func(seg_hat, labels.type(torch.float32))
@@ -75,37 +75,10 @@ class EngineModule(pl.LightningModule):
         pass
 
     def configure_optimizers(self):
-        optimizer = get_optimizer(self.config.training.optimizer, self.parameters())
-        scheduler_config = self.config.training.scheduler
-        if scheduler_config is not None:
-            scheduler = get_scheduler(scheduler_config, optimizer)
-            return [optimizer], [scheduler]
-        else:
-            return optimizer
+        optimizer_g = torch.optim.Adam(list(self.G_A2B.parameters())+list(self.netG_B2A.parameters()),
+                                       lr=self.config.training.lr, betas=(0.5, 0.999))
+        optimizer_d = torch.optim.Adam(list(self.D_A.parameters())+list(self.D_B.parameters()),
+                                       lr=self.config.training.lr, betas=(0.5, 0.999))
+        return optimizer_g, optimizer_d
 
 
-def get_optimizer(optim_config: DictConfig, params):
-    name = optim_config.name
-    lr = optim_config.lr
-
-    if name == 'sgd':
-        return torch.optim.SGD(params, lr=lr)
-    elif name == 'adam':
-        return torch.optim.Adam(params, lr=lr)
-    else:
-        raise ValueError(f'{name} not in optimizers')
-
-
-def get_scheduler(scheduler_config, optimizer):
-    name = scheduler_config.name
-    monitor = scheduler_config.monitor
-
-    if name == 'plateau':
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
-                                                               mode=scheduler_config.mode,
-                                                               patience=scheduler_config.patience,
-                                                               factor=scheduler_config.factor,
-                                                               min_lr=scheduler_config.min_lr)
-        return dict(scheduler=scheduler, monitor=monitor)
-    else:
-        raise ValueError(f'{name} not in schedulers')
