8c9041d51daa6a50dcc71c932b6501c7a3b7c54a
Janek <jan.piotr.latko@gmail.com>
2021-06-18 10:52:43 +0200

    data

--- project_2/src/data.py
+++ project_2/src/data.py
@@ -7,7 +7,8 @@ import shutil
 from PIL import Image
 import torch
 import gdown
-import matplotlib.pyplot as plt
+from pytorch_lightning.trainer.supporters import CombinedLoader
+
 
 def download_url(url, save_path, chunk_size=128):
     gdown.download(url, save_path, quiet=False)
@@ -58,21 +59,66 @@ class ImageDataset(Dataset):
         self.fnames = list(os.listdir(self.img_path))
         self.img_transform = img_transform
 
+    def make_copy(self):
+        return ImageDataset(folder_path=self.img_path, img_transform=self.img_transform)
+
     def __len__(self):
         return len(self.fnames)
 
     def __getitem__(self, idx):
         fname = self.fnames[idx]
+
+        if fname is None:
+            return []
+
         img = self.img_transform(Image.open(os.path.join(self.img_path, fname)))
         return img
 
+
+def pad_dataset(dataset, length):
+    while len(dataset.fnames) < length:
+        dataset.fnames.append(None)
+
+
+def split_dataset(dataset, valid_transform, split=0.8, seed=2312):
+    all_fnames = dataset.fnames
+    n = len(all_fnames)
+    val_size = int((1 - split) * n)
+    np.random.seed(seed)
+    all_ind = list(range(n))
+    val_ind = np.random.choice(all_ind, size=val_size, replace=False)
+    val_dataset = dataset.make_copy()
+    val_dataset.img_transform = valid_transform
+
+    val_dataset.fnames = [all_fnames[i] for i in val_ind]
+    dataset.fnames = [all_fnames[i] for i in list(set(all_ind) - set(val_ind))]
+    return dataset, val_dataset
+
+
 def get_dataloaders(size, train_augmentation, batch_size, url, data_path):
     train_transform, valid_transform = get_transforms(size, [])
-    train_set, valid_set, test_set = get_dataset(url, data_path, train_transform, valid_transform)
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
-    valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=False, num_workers=2)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
-    return train_loader, valid_loader, test_loader
+    train_horse, train_zebra, test_horse, test_zebra = get_dataset(url, data_path, train_transform, valid_transform)
+    # we skip it for now
+    # train_horse, val_horse = split_dataset(train_horse, valid_transform, split=0.8, seed=2312)
+    # train_zebra, val_zebra = split_dataset(train_zebra, valid_transform, split=0.8, seed=2312)
+    train_loader_horse = DataLoader(train_horse, batch_size=batch_size, shuffle=True, num_workers=2)
+    train_loader_zebra = DataLoader(train_zebra, batch_size=batch_size, shuffle=True, num_workers=2)
+    train_loaders = {
+        'horse': train_loader_horse,
+        'zebra': train_loader_zebra,
+    }
+    # pad horses with None to the length of zebra
+    pad_dataset(test_horse, len(test_zebra))
+
+    test_loader_horse = DataLoader(test_horse, batch_size=batch_size, shuffle=False, num_workers=2)
+    test_loader_zebra = DataLoader(test_zebra, batch_size=batch_size, shuffle=False, num_workers=2)
+
+    # combine dataloaders
+    test_loaders = CombinedLoader({
+        'horse': test_loader_horse,
+        'zebra': test_loader_zebra,
+    }, "max_size_cycle")
+    return train_loaders, test_loaders
 
 
 def get_transforms(size, train_augmentation):
@@ -98,6 +144,3 @@ def get_transforms(size, train_augmentation):
 
     return train_transform, valid_transform
 
-def plot_image(img):
-    plt.imshow(img.moveaxis(0, -1))
-    plt.axis('off')
