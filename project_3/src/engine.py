c5491602b40cbb6a31e5f5dfdcbc577341679a1f
Janek <jan.piotr.latko@gmail.com>
2021-06-18 17:36:49 +0200

    val step

--- project_3/src/engine.py
+++ project_3/src/engine.py
@@ -29,6 +29,7 @@ class EngineModule(pl.LightningModule):
     def forward(self, x):
         return self.model(x)
 
+
     def training_step(self, batch, batch_idx):
         real_h, real_z = batch['horse'], batch['zebra']
         g_opt, d_opt, = self.optimizers()
@@ -102,7 +103,64 @@ class EngineModule(pl.LightningModule):
         pass
 
     def validation_step(self, batch, batch_idx):
-        pass
+        with torch.no_grad():
+            real_h, real_z = batch['horse'], batch['zebra']
+            print(real_h.shape, real_h.size())
+
+            if len(real_h.size()) == 0:
+                return
+
+            g_opt, d_opt, = self.optimizers()
+            # TODO: normalizatoin
+            # --------------------- #
+            #   Fit discriminator   #
+            # --------------------- #
+
+            # Real loss
+            pred_h_real, pred_z_real = self.d_h(real_h), self.d_z(real_z)
+            loss_h_real = self.loss.criterion_GAN(pred_h_real, self.target_real)
+            loss_z_real = self.loss.criterion_GAN(pred_z_real, self.target_real)
+            del pred_h_real, pred_z_real
+
+            # Fake loss
+            fake_h, fake_z = self.g_z2h(real_z), self.g_h2z(real_h)
+            pred_h_fake, pred_z_fake = self.d_h(fake_h), self.d_z(fake_z)
+            loss_h_fake = self.loss.criterion_GAN(pred_h_fake, self.target_fake)
+            loss_z_fake = self.loss.criterion_GAN(pred_z_fake, self.target_fake)
+            del fake_h, fake_z, pred_h_fake, pred_z_fake
+
+            # Total loss
+            loss_d = loss_h_real+loss_z_real+loss_h_fake+loss_z_fake
+
+            # --------------------- #
+            #     Fit generator     #
+            # --------------------- #
+            # Identity loss
+            same_h, same_z = self.g_z2h(real_h), self.g_h2z(real_z)
+            loss_identity_h = self.loss.criterion_identity(same_h, real_h)
+            loss_identity_z = self.loss.criterion_identity(same_z, real_z)
+            del same_h, same_z
+
+            # GAN loss
+            fake_h, fake_z = self.g_z2h(real_z), self.g_h2z(real_h)
+            pred_fake_h, pred_fake_z = self.d_h(fake_h), self.d_z(fake_z)
+            loss_gan_z2h = self.loss.criterion_GAN(pred_fake_h, self.target_real)
+            loss_gan_h2z = self.loss.criterion_GAN(pred_fake_z, self.target_real)
+            del pred_fake_h, pred_fake_z
+
+            # Cycle loss
+            recovered_h, recovered_z = self.g_z2h(fake_z), self.g_h2z(fake_h)
+            loss_cycle_hzh = self.loss.criterion_cycle(recovered_h, real_h)
+            loss_cycle_zhz = self.loss.criterion_cycle(recovered_z, real_z)
+            del fake_h, fake_z, recovered_h, recovered_z
+
+            # Total loss
+            loss_g = loss_identity_h + loss_identity_z + loss_gan_z2h + loss_gan_h2z + loss_cycle_hzh + loss_cycle_zhz
+
+            self.log('val_loss_g', loss_g, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+            self.log('val_loss_d', loss_d, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+            # for tracking general progress
+            self.log('val_loss_sum', loss_d + loss_g, on_step=False, on_epoch=True, prog_bar=True, logger=True)
 
     def validation_epoch_end(self, outputs: list):
         make_plots(self.test_dataset_horse, self.g_h2z, self.g_z2h, self.device, n=4, current_epoch=self.current_epoch, suffix='_h2z')
