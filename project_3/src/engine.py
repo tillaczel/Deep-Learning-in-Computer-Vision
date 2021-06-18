955b8a81e3b44e6c32610415df09f318b8e76f28
Till <tillaczel@gmail.com>
2021-06-18 14:38:23 +0200

    generator step

--- project_3/src/engine.py
+++ project_3/src/engine.py
@@ -13,8 +13,13 @@ class EngineModule(pl.LightningModule):
     def __init__(self, config: DictConfig):
         super().__init__()
         self.config = config
-        self.G_A2B, self.G_B2A, self.D_A, self.D_B = get_networks()
-        self.losses = Losses()
+        self.g_h2z, self.g_z2h, self.d_h, self.d_z = get_networks()
+        self.loss = Losses()
+
+        # TODO: batch_size to config
+        batch_size = 64
+        self.target_real = torch.autograd.Variable(torch.ones(batch_size, device=self.device), requires_grad=False)
+        self.target_fake = torch.autograd.Variable(torch.zeros(batch_size, device=self.device), requires_grad=False)
 
         self.automatic_optimization = False
 
@@ -26,10 +31,36 @@ class EngineModule(pl.LightningModule):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
-        print(batch)
-
+        real_h, real_z = batch['horse'], batch['zebra']
         g_opt, d_opt, = self.optimizers()
-        augment_images = DiffAugment(images, policy='color,translation,cutout')
+        # augment_images = DiffAugment(images, policy='color,translation,cutout')
+
+        # ----------------- #
+        #   Fit generator   #
+        # ----------------- #
+        # Identity loss
+        same_h, same_z = self.g_z2h(real_h), self.g_h2z(real_z)
+        loss_identity_h = self.loss.criterion_identity(same_h, real_h)
+        loss_identity_z = self.loss.criterion_identity(same_z, real_z)
+
+        # GAN loss
+        fake_h, fake_z = self.g_z2h(real_z), self.g_h2z(real_h)
+        pred_fake_h, pred_fake_z = self.d_h(fake_h), self.d_z(fake_z)
+        loss_gan_z2h = self.loss.criterion_GAN(pred_fake_h, self.traget_real)
+        loss_gan_h2z = self.loss.criterion_GAN(pred_fake_z, self.traget_real)
+
+        # Cycle loss
+        recovered_h, recovered_z = self.g_z2h(fake_z), self.g_h2z(fake_h)
+        loss_cycle_hzh = self.loss.criterion_cycle(recovered_h, real_h)
+        loss_cycle_zhz = self.loss.criterion_cycle(recovered_z, real_z)
+
+        # Total loss
+        loss_g = loss_identity_h + loss_identity_z + loss_gan_z2h + loss_gan_h2z + loss_cycle_hzh + loss_cycle_zhz
+
+        # Step
+        g_opt.zero_grad()
+        loss_g.backward()
+        g_opt.step()
 
         self.log('loss', loss, on_step=False, on_epoch=True,
                  prog_bar=False, logger=True)
@@ -42,16 +73,15 @@ class EngineModule(pl.LightningModule):
         pass
 
     def validation_step(self, batch, batch_idx):
-        print(batch['horse'].shape)
         pass
 
     def validation_epoch_end(self, outputs: list):
         pass
 
     def configure_optimizers(self):
-        optimizer_g = torch.optim.Adam(list(self.G_A2B.parameters())+list(self.G_B2A.parameters()),
+        optimizer_g = torch.optim.Adam(list(self.g_h2z.parameters()) + list(self.g_z2h.parameters()),
                                        lr=self.config.training.optimizer.lr, betas=(0.5, 0.999))
-        optimizer_d = torch.optim.Adam(list(self.D_A.parameters())+list(self.D_B.parameters()),
+        optimizer_d = torch.optim.Adam(list(self.d_h.parameters()) + list(self.d_z.parameters()),
                                        lr=self.config.training.optimizer.lr, betas=(0.5, 0.999))
         return optimizer_g, optimizer_d
 
